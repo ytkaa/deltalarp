7f7f65016d0996d4e867dc1ce5e0cfd7bfb9574c
The4Pikmin <65878512+The4Pikmin@users.noreply.github.com>
2026-07-19 19:29:06 -0400

    kipkip

---
+++ scripts/battle/waves/kipkipthrow.lua
@@ -0,0 +1,38 @@
+local kipkipthrow, super = Class(Wave)
+
+function kipkipthrow:init()
+    super.init(self)
+
+    -- The duration of our wave, in seconds. (Defaults to `5`)
+    self.time = 10
+    kipkipThrowSpeed = 0.5
+    kipkipBulletSpeed = 4
+end
+
+function kipkipthrow:onStart()
+    local kipkip = self:getAttackers()[1] --grab enemy pos
+    --local x = SCREEN_WIDTH / 2 
+    --local y = SCREEN_HEIGHT / 2 position is the middle of the screen
+
+    if kipkip.kipkipAgitated then
+        kipkipThrowSpeed = 0.25
+        kipkipBulletSpeed = 6
+    end
+    self.timer:everyInstant(kipkipThrowSpeed, function()
+        local x, y = kipkip:getRelativePos(kipkip.width/2, kipkip.height/2) -- position is the enemy pos
+        local kipkipbullet = Utils.pick({"bullets/kipkip/brick", "bullets/kipkip/rake", "bullets/kipkip/watering_can", "bullets/kipkip/flower"}) --grabs random sprite from the following!
+        local target_angle = MathUtils.angle(x, y, Game.battle.soul.x, Game.battle.soul.y)
+        local bullet = self:spawnBullet(kipkipbullet, x, y)
+        bullet.rotation = target_angle + math.rad(MathUtils.random(-20, 20))
+        bullet.physics.match_rotation = true
+        bullet.physics.speed = kipkipBulletSpeed
+    end)
+end
+
+function kipkipthrow:update()
+    -- code here gets called every frame
+
+    super.update(self)
+end
+
+return kipkipthrow
