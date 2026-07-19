4b5b1285adfad620263c639cbdc22614c44801ea
The4Pikmin <65878512+The4Pikmin@users.noreply.github.com>
2026-07-19 19:28:26 -0400

    kipkip

---
+++ scripts/battle/encounters/kipkip.lua
@@ -0,0 +1,17 @@
+local Kipkip, super = Class(Encounter)
+
+function Kipkip:init()
+    super.init(self)
+
+    -- Text displayed at the bottom of the screen at the start of the encounter
+    self.text = "* Kipkip sprouts from the ground!"
+
+    -- Battle music ("battle" is rude buster)
+    self.music = "battle"
+    -- Enables the purple grid battle background
+    self.background = true
+
+    self:addEnemy("kipkip")
+end
+
+return Kipkip
