fef3e5886c025180d6e711fd10c590dbfb5e9dba
ytkaa <loganpaulcoolfan@gmail.com>
2026-07-20 22:34:58 -0500

    Major mistakes and missteps

---
+++ scripts/battle/encounters/colonel_squad.lua
@@ -0,0 +1,21 @@
+local Colonel, super = Class(Encounter)
+
+function Colonel:init()
+    super.init(self)
+
+    -- Text displayed at the bottom of the screen at the start of the encounter
+    self.text = "* The squad conducts a special military operation!"
+
+    -- Battle music ("battle" is rude buster)
+    self.music = "petal8"
+    -- Enables the purple grid battle background
+    self.background = true
+
+    self:addEnemy("colonel")
+    self:addEnemy("colonel")
+    self:addEnemy("colonel")
+    self:addEnemy("colonel")
+    self:addEnemy("colonel")
+end
+
+return Colonel
