b8b6213fee32ae1b5d6a54a9d13c449222cba816
ytkaa <loganpaulcoolfan@gmail.com>
2026-08-06 21:43:14 -0500

    Unlocked new battle sprites for vess

--- scripts/data/actors/vess.lua
+++ scripts/data/actors/vess.lua
@@ -31,16 +31,27 @@ function actor:init()
         ["battle/idle"] = {"battle/idle", 1/6, true},
 
         ["battle/attack"]       = {"battle/attack", 1/15, false},
-        --["battle/act"]          = {"battle/act", 1/15, false},
-        --["battle/spell"]        = {"battle/act", 1/15, false},
-        --["battle/item"]         = {"battle/item", 1/12, false, next="battle/idle"},
-        --["battle/spare"]        = {"battle/act", 1/15, false, next="battle/idle"},
+        ["battle/act"]          = {"battle/act", 1/15, false},
+        ["battle/spell"]        = {"battle/act", 1/15, false},
+        ["battle/item"]         = {"battle/item", 1/12, false, next="battle/idle"},
+        ["battle/spare"]        = {"battle/act", 1/15, false, next="battle/idle"},
 
         ["battle/attack_ready"] = {"battle/attackready", 0.2, true},
-        --["battle/act_ready"]    = {"battle/actready", 0.2, true},
-        --["battle/spell_ready"]  = {"battle/actready", 0.2, true},
-        --["battle/item_ready"]   = {"battle/itemready", 0.2, true},
-        --["battle/defend_ready"] = {"battle/defend", 1/15, false},
+        ["battle/act_ready"]    = {"battle/actready", 0.2, true}, --Kris 0.2
+        ["battle/spell_ready"]  = {"battle/actready", 0.2, true},
+        ["battle/item_ready"]   = {"battle/itemready", 0.2, true},
+        ["battle/defend_ready"] = {"battle/defend", 1/15, false},
+
+        ["battle/act_end"]      = {"battle/actend", 1/15, false, next="battle/idle"},
+
+        --["battle/hurt"]         = {"battle/hurt", 1/15, false, temp=true, duration=0.5},
+        --["battle/defeat"]       = {"battle/defeat", 1/15, false},
+        --["battle/swooned"]      = {"battle/defeat", 1/15, false},
+
+        --["battle/transition"]   = {"sword_jump_down", 0.2, true},
+        ["battle/intro"]        = {"battle/attack", 1/15, false},
+        --["battle/victory"]      = {"battle/victory", 1/10, false},
+        --["battle/transition_out"] = {"battle/transition_out", 1/15, false},
     }
 
     self.offsets = {
@@ -55,12 +66,18 @@ function actor:init()
 
         ["battle/attack"] = {-8, -6},
         ["battle/attackready"] = {-8, -6},
-        --["battle/act"] = {-6, -6},
-        --["battle/actend"] = {-6, -6},
-        --["battle/actready"] = {-6, -6},
-        --["battle/item"] = {-6, -6},
-        --["battle/itemready"] = {-6, -6},
-        --["battle/defend"] = {-5, -3},
+        ["battle/act"] = {-6, -6},
+        ["battle/actend"] = {-6, -6},
+        ["battle/actready"] = {-6, -6},
+        ["battle/item"] = {-6, -6},
+        ["battle/itemready"] = {-6, -6},
+        ["battle/defend"] = {-5, -3},
+
+        --["battle/defeat"] = {-8, -5},
+        --["battle/hurt"] = {-5, -6},
+
+        ["battle/intro"] = {-8, -9},
+        --["battle/victory"] = {-3, 0},
 
         -- Climb offsets
         ["climb/climbing"] = {-5, -15},
